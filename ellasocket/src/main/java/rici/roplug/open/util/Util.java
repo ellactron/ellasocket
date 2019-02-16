fa6b91fc28d5baee631e001574fce0693bd8d1e1
Jeff <jeffwji@gmail.com>
2019-02-16 12:48:16 -0500

    Update address

--- ellasocket/src/main/java/rici/roplug/open/util/Util.java
+++ ellasocket/src/main/java/rici/roplug/open/util/Util.java
@@ -4,7 +4,7 @@ public class Util{
 	/**
 	 * 服务器IP地址
 	 */
-	public static String SERVER_IP = "23.245.144.70";
+	public static String SERVER_IP = "www.ellactron.com";
 //	public static String SERVER_IP = "115.28.216.177";//rici 服务器地址
 
 }
